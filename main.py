22676473f1b514287fc72b1a52a19251e213903c
kimpenguin <kpeng@nycopportunity.nyc.gov>
2019-05-16 13:16:07 -0400

    updating download and extraction, overlaying, dissolving, and combining shapefiles into final geoJSON

--- main.py
+++ main.py
@@ -1,48 +1,146 @@
-## This script makes the following assumptions:
-## 1. The Census will not update their domain or their file system
+# Downloads, extracts, and merges US Census shapefiles
+# to generate a usable zip code geodataframe for NYC
 
 ## Importing necessary libraries and files
-
 import os
 import datetime
 import ndash_func
-from nycdatasets import *
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from glob import glob
 
 # Get the current working directory
 os.getcwd()
+print(os.getcwd())
 
 cur_date = datetime.datetime.now()
 
-#####
+# census values for state and counties
+fips='36'
+counties = [
+'Bronx', 
+'Kings', 
+'New York', 
+'Queens', 
+'Richmond']
+
+###############
+# COUNTY Download and Extraction
+county_url=ndash_func.findFile(cur_date.year, 'COUNTY')
+ndash_func.downloadFile(county_url)
+if ndash_func.fileExists(county_url):
+  county_file=ndash_func.extractFile(county_url)
+  print(county_file)
+
+county_file='tl_2018_us_county'
+county_shp = gpd.read_file(county_file + '.shp')
+
+# EXTRACT: only the NYC counties that fall in NYS
+nyc_counties=county_shp[(county_shp['NAME'].isin(counties)) & (county_shp['STATEFP']==fips) ]
+
+###############
+# PUBLIC USE MICRODATA AREAS (PUMA) Download and Extraction
+puma_url=ndash_func.findFile(cur_date.year, 'PUMA')
+ndash_func.downloadFile(puma_url)
+if ndash_func.fileExists(puma_url):
+  puma_file=ndash_func.extractFile(puma_url)
+  print(puma_file)
+
+puma_file='tl_2018_36_puma10'
+puma_shp = gpd.read_file(puma_file + '.shp')
+
+# OVERLAY: Overlay PUMA and NYC Counties
+nyc_puma = gpd.overlay(puma_shp, nyc_counties, how='intersection')
+
+###############
 # TIGER Zip Code Tabulation Areas Census Data Set Download and Extraction
 zcta_url=ndash_func.findFile(cur_date.year, 'ZCTA5')
-zcta_fn=ndash_func.downloadFile(zcta_url)
+ndash_func.downloadFile(zcta_url)
+if ndash_func.fileExists(zcta_url):
+  zcta_file=ndash_func.extractFile(zcta_url)
+  print(zcta_file)
 
-# download the tiger files and pass the output name
-if zcta_fn != '':
-	# extract the file
-	ndash_func.extractFile(zcta_fn)
-else:
-	print('Something went wrong. Inform the team.')
+zcta_file='tl_2018_us_zcta510'
+zcta_shp = gpd.read_file(zcta_file + '.shp')
 
-#####
-# TIGER County Census Data Set Download and Extraction
-county_url=ndash_func.findFile(cur_date.year, 'COUNTY')
-county_fn=ndash_func.downloadFile(county_url)
+###############
+# MERGING DATASETS
+# OVERLAY: Overlaying PUMA and counties on top of ZCTA
+nyc_zcta_puma = gpd.overlay(nyc_puma, zcta_shp, how='intersection')
 
-# download the tiger files and pass the output name
-if county_fn != '':
-	# extract the file
-	ndash_func.extractFile(county_fn)
-else:
-	print('Something went wrong. Inform the team.')
+# DISSOLVE: by zipcode
+nyc_zcta_puma_dissolved = nyc_zcta_puma.dissolve(by='GEOID10_2')
 
-#####
-# Neighborhood Tabulation Areas (NTA) Download and Extraction
-# Download the data from the open data portal
-nta_fn=ndash_func.downloadFile(nta_url)
-if nta_fn !='':
-	# extract the file
-	ndash_func.extractFile(nta_fn)
-else:
-	print('Something went wrong. Inform the team.')
+# FILTER: keeping relevant columns
+nyc_zipcodes=nyc_zcta_puma_dissolved.filter([
+'ZCTA5CE10',
+'NAMELSAD10',
+'NAME',
+'STATEFP',
+'COUNTYFP',
+'COUNTYNS',
+'PUMACE10',
+'ALAND10_2',
+'AWATER10_2',
+'INTPTLAT10_2',
+'INTPTLON10_2',
+'geometry'
+])
+
+# UPDATE: Borough Names
+nyc_zipcodes['NAME'] = nyc_zipcodes['NAME'].str.replace('Kings','Brooklyn')
+nyc_zipcodes['NAME'] = nyc_zipcodes['NAME'].str.replace('Richmond','Staten Island')
+
+# SPLIT: NAMESLAD into Community Districts and Neighborhoods
+nyc_zipcodes[['NAMELSAD10','NBH']] = nyc_zipcodes['NAMELSAD10'].str.split('--',expand=True)
+nyc_zipcodes['NBH'] = nyc_zipcodes['NBH'].str.replace(' PUMA','')
+
+# RENAME: columns
+nyc_zipcodes.rename(columns={
+  'NAMELSAD10':'CD',
+  'NAME':'BORO',
+  'ALAND10_2' : 'ALAND10',
+  'AWATER10_2' : 'AWATER10',
+  'INTPTLAT10_2' : 'INTPTLAT10',
+  'INTPTLON10_2' : 'INTPTLON10'
+  }, inplace=True)
+
+# REARRANGE: column order
+nyc_zipcodes = nyc_zipcodes[[
+'ZCTA5CE10',
+'BORO',
+'CD',
+'NBH',
+'STATEFP',
+'COUNTYFP',
+'COUNTYNS',
+'PUMACE10',
+'ALAND10',
+'AWATER10',
+'INTPTLAT10',
+'INTPTLON10',
+'geometry'
+]]
+
+# PLOT: the merged dataset
+nyc_zipcodes.plot()
+plt.show()
+
+# EXPORT: GeoJSON
+nyc_zipcodes.to_file("nyco-nyc_zipcodes.geojson", driver='GeoJSON')
+
+###############
+# CLEAN: remove the downloaded files
+ext = [
+'*.cpg',
+'*.dbf',
+'*.prj',
+'*.shp',
+'*.shx',
+'*.zip',
+'*.xml'
+]
+
+for e in ext:
+  for file in glob(e):
+    os.remove(file)
--- ndash_func.py
+++ ndash_func.py
@@ -1,6 +1,7 @@
 # Functions to retrieve, extract, and transform US Census Bureau
 # Zip Code Tabulation Data
 
+# import packages
 import urllib2
 import requests
 import urllister
@@ -8,7 +9,7 @@ import zipfile
 import sys
 import os, os.path
 
-#####
+###############
 # Checks to see if there is a zip for the current year
 # If it does not exist, then decrements year until a
 # file is found
@@ -20,13 +21,16 @@ def findFile(year, dataset):
     
     while success==False:
         try:
-			con=urllib2.urlopen(data_path + str(year) + "/" + dataset)
+          print(data_path + str(year) + "/" + dataset)
+          con=urllib2.urlopen(data_path + str(year) + "/" + dataset)
         except urllib2.HTTPError, e:
-            print(str(e.code) + " connection failed! Decrementing year and trying again.")
-            year = year -1
+          print(e)
+          print("Decrementing year and trying again.")
+          year = year -1
         except urllib2.URLError, e:
-            print(str(e.args)  + " connection failed! Decrementing year and trying again.")
-            year = year -1
+          print(e)
+          print("Decrementing year and trying again.")
+          year = year -1
         else:
             print("Connection a success!")
             success=True
@@ -36,57 +40,71 @@ def findFile(year, dataset):
             parser.close()
             zipfile=[s for s in parser.urls if ".zip" in s]
 
+    # if there are more than 1 zip files, look for NYS
+    if len(zipfile) > 1:
+      ny_i = [i for i, s in enumerate(zipfile) if '_36_' in s][0]
+      zipfile = zipfile[ny_i]
+
     file_url=data_path + str(year) + "/"+ dataset + "/" + ''.join(zipfile)
 
     return file_url
 
-#####
+###############
+# Checks to see if the file exists
+def fileExists(file_url):
+  filename=file_url.split('/')[-1]
+  if os.path.isfile(filename):
+    return True
+  else:
+    return False
+
+###############
 # Takes a url and downloads the file to the local repository
 def downloadFile(file_url):
-	success=False
+    counter=0
 
-	if file_url.endswith('.zip'):
-		filename=downloadZip(file_url)
-	else:
-		filename=downloadUrl(file_url)
-	
-	return filename
+    while not fileExists(file_url):
+        if file_url.endswith('.zip'):
+          filename=downloadZip(file_url)
+        else:
+          filename=downloadUrl(file_url)
 
-#####
+        if counter > 3:
+          print('There is a problem downloading. Exiting.')
+          break
+        counter+=1
+
+###############
 # Used when the URL is a direct source of the file
 def downloadZip(file_url):
 	con=urllib2.urlopen(file_url)
 
 	filename=file_url.split('/')[-1]
 
-	if os.path.isfile(filename):
-		print(filename + ' already exists!')
+	print('Downloading ' + filename)
+	total_size = con.info().getheader('Content-Length').strip()
+	total_size = int(total_size)
+
+	bytes_so_far = 0
+
+	save_file=open(filename, "wb")
+
+	while bytes_so_far < total_size:
+	    buff = con.read(8192)
+	    save_file.write(buff)
+	    bytes_so_far += len(buff)
+
+	    percent = float(bytes_so_far) / total_size
+	    percent = round(percent*100, 2)
+	    sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" % (bytes_so_far, total_size, percent))
+
+	save_file.close()
+
+	if bytes_so_far == total_size:
+		print(filename + ' successfully downloaded!')
 		return filename
-	else:
-		print('Downloading ' + filename)
-		total_size = con.info().getheader('Content-Length').strip()
-		total_size = int(total_size)
 
-		bytes_so_far = 0
-
-		save_file=open(filename, "wb")
-
-		while bytes_so_far < total_size:
-		    buff = con.read(8192)
-		    save_file.write(buff)
-		    bytes_so_far += len(buff)
-
-		    percent = float(bytes_so_far) / total_size
-		    percent = round(percent*100, 2)
-		    sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" % (bytes_so_far, total_size, percent))
-
-		save_file.close()
-
-		if bytes_so_far == total_size:
-			print(filename + ' successfully downloaded!')
-			return filename
-
-#####
+###############
 # Used when the URL triggers a download of a zip file
 def downloadUrl(file_url):
 	req=requests.get(file_url)
@@ -108,24 +126,29 @@ def downloadUrl(file_url):
 
 		return filename
 
-
-#####
+###############
 # Extract the zip file
-def extractFile(filename):
-	current_dir= os.getcwd()
+def extractFile(file_url):
+  current_dir= os.getcwd()
 
-	output_dir=os.path.join(current_dir, os.path.splitext(filename)[0])
+  filename=file_url.split('/')[-1];
+
+  # output_dir=os.path.join(current_dir, os.path.splitext(filename)[0])
+  output_dir=os.path.join(current_dir, 'shapefiles')
 	
-	if not os.path.exists(output_dir):
-	    os.makedirs(output_dir)
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 	# check to see that the file exists, or exit
-	if not os.path.isfile(filename):
-		print('Cannot find + ' + filename)
-		sys.exit()
+  if not os.path.isfile(filename):
+    print('Cannot find + ' + filename)
+    sys.exit()
 
-	print('Beginning extraction')
-	
-	zipfile.ZipFile(filename).extractall(output_dir)
-	print(filename + ' extraction complete!')
+  print('Beginning extraction')
+
+  # zipfile.ZipFile(filename).extractall(output_dir)
+  zipfile.ZipFile(filename).extractall()
+  print(filename + ' extraction complete!')
+
+  return filename.split('.zip')[0]
 
